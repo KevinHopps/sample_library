8b6df54b24d79913a1614c9fe37bcc14ce629900
Kevin Hopps <khopps@adobe.com>
2025-02-20 14:24:44 -0600

    add some sources; add cmake config file

---
+++ tests/src/mylib_test.cpp
@@ -0,0 +1,14 @@
+#include <mylib/verison.hpp>
+
+#include <gtest/gtest.h>
+
+namespace mylib {
+    namespace tests {
+
+        TEST(basic_tests, version_test) {
+            EXPECT_EQ(version_major(), MYLIB_VERSION_MAJOR);
+            EXPECT_EQ(version_minor(), MYLIB_VERSION_MINOR);
+        }
+
+    }
+}
